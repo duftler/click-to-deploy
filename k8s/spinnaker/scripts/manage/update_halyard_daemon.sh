b2f3d44c2c15647307af858b3ca89150fae4fb05
Ola Karlsson <olakar@google.com>
2019-05-30 15:31:10 +1000

    Set HALYARD_VERSION to something useful if not set in properties.

--- k8s/spinnaker/scripts/manage/update_halyard_daemon.sh
+++ k8s/spinnaker/scripts/manage/update_halyard_daemon.sh
@@ -8,4 +8,7 @@ source ~/click-to-deploy/k8s/spinnaker/scripts/install/properties
 
 bold "Updating halyard daemon..."
 
+if [ -z "$HALYARD_VERSION"];
+	HALYARD_VERSION="stable"
+
 kubectl set image statefulset spin-halyard -n halyard halyard-daemon=gcr.io/spinnaker-marketplace/halyard:$HALYARD_VERSION
