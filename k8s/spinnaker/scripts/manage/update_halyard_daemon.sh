7191664e9eaf6876d6b370338df3f77379e380a2
Ola Karlsson <olakar@google.com>
2019-05-31 11:23:33 +1000

    Fail instead of setting a version.

--- k8s/spinnaker/scripts/manage/update_halyard_daemon.sh
+++ k8s/spinnaker/scripts/manage/update_halyard_daemon.sh
@@ -9,6 +9,7 @@ source ~/click-to-deploy/k8s/spinnaker/scripts/install/properties
 bold "Updating halyard daemon..."
 
 if [ -z "$HALYARD_VERSION"];
-	HALYARD_VERSION="stable"
+	bold "HALYARD_VERSION not set..."
+	exit 1
 
 kubectl set image statefulset spin-halyard -n halyard halyard-daemon=gcr.io/spinnaker-marketplace/halyard:$HALYARD_VERSION
