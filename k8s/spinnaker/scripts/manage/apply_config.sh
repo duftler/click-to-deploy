9b6afb20cea3dde1600e024b372859cb1449b4e0
Matt Duftler <duftler@google.com>
2019-06-04 09:16:56 -0400

    Removing everything since it has all been moved to GoogleCloudPlatform/spinnaker-for-gcp.

--- k8s/spinnaker/scripts/manage/apply_config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-HALYARD_POD=spin-halyard-0
-
-kubectl exec $HALYARD_POD -n halyard -- bash -c 'hal deploy apply'
-
-~/click-to-deploy/k8s/spinnaker/scripts/manage/deploy_application_manifest.sh
